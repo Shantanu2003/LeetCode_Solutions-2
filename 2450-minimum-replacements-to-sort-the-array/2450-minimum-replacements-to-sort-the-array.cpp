781f3435a5776e9e6d95e47f299b7fa889c72fe1
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2024-07-15 23:41:54 +0530

    Time: 85 ms (58.39%), Space: 57.3 MB (9.66%) - LeetHub

---
+++ 2450-minimum-replacements-to-sort-the-array/2450-minimum-replacements-to-sort-the-array.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    long long minimumReplacement(vector<int>& nums) {
+        long long answer = 0;
+        long long nums_elements;
+        for(int  i = nums.size()-2; i>=0; i--){
+            if(nums[i]<= nums[i+1]){
+                continue;
+            }
+            long long nums_elements = ((nums[i] + nums[i + 1] - 1LL) / (nums[i + 1]));
+            answer += nums_elements-1;
+
+            nums[i] = nums[i]/nums_elements;
+        }
+        return answer;
+    }
+};
+// 9 > 3
+// 9 + 3  - 1/ 3
+// 4 
+// answer = 3
+
+// i = 9 / 4 = 2
+
+// 3 2 3
+
+// 3 + 2 -1 / 2 = 2
+// aswer = 4
