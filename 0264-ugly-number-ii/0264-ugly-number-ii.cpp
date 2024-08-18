c2c7046ff4304cb6dd87d49acb16269f020f898e
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2024-08-18 15:43:17 +0530

    Time: 4 ms (60.74%), Space: 9.8 MB (56.48%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -1,46 +1,21 @@
 class Solution {
 public:
-    bool isPrime(int num){
-        if(num <= 1)
-            return false;
-        if(num == 2 || num == 3)
-            return true;
-        if(num % 2 == 0 || num % 3 == 0)
-            return false;
-
-        for(int i = 5; i * i <= num; i += 6){
-            if(num % i == 0 || num % (i + 2) == 0)
-                return false;
-        }
-        return true;
-    }
-
-    bool isValid(int num){
-        for(int i = 2; i <= num; i++){
-            if(num % i == 0){
-                if(isPrime(i)){
-                    if(i != 2 && i != 3 && i != 5)
-                        return false;
-                }
-            }
-        }
-        return true;
-    }
-
     int nthUglyNumber(int n) {
-        if(n == 1)
-            return 1;
+        vector<int>ans(n+1, 0);
+        ans[1] = 1;
+   
+        int i2 = 1, i3 = 1, i5 = 1; // Pointers for multiples of 2, 3, and 5
 
-        int i = 2;
-        int count = 1; // 1 is already counted as the first ugly number
+        for (int i = 2; i <= n; ++i) {
+            int nextUgly = min({2 * ans[i2], 3 * ans[i3], 5 * ans[i5]});
+            ans[i] = nextUgly;
 
-        while(n > count){
-            if(isValid(i)){
-                count++;
-            }
-            i++;
+            // Update pointers based on the chosen ugly number
+            if (nextUgly == 2 * ans[i2]) i2++;
+            if (nextUgly == 3 * ans[i3]) i3++;
+            if (nextUgly == 5 * ans[i5]) i5++;
         }
 
-        return i - 1; // Because the loop increments i before checking the condition
+        return ans[n];
     }
 };
